7c5ad247dd0e0042f67eb8533337cf66571e864c
Your Name <bilalzainab123agmail.com>
2025-01-23 20:09:40 +0500

    learn event handling

---
+++ event handling/src/App.jsx
@@ -0,0 +1,40 @@
+
+
+function App() {
+ 
+// function handlechange(e){
+//  console.log("value till now",e.target.value)
+// }
+// function handlesubmit(e){
+//   e.preventDefualt(),
+//   alert('form submit o gaya kya?')
+// }
+//   return (
+//     <>
+//     <form onSubmit={handlesubmit}> 
+//       <input type="text" onChange={handlechange}></input>
+//     <button>click</button>
+//     </form>
+      
+//     </>
+//   )
+ 
+//   function handleClick() {
+//     let bodyStyle = document.body.style;
+//     if (bodyStyle.backgroundColor === 'black') {
+//       bodyStyle.backgroundColor = 'white';
+//     } else {
+//       bodyStyle.backgroundColor = 'black';
+//     }
+//   }
+
+//   return (
+//     <button onClick={handleClick()}>
+//       Toggle the lights
+//     </button>
+//   );
+// }
+
+
+
+export default App
